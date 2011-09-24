05bbf62965057faeacae8b0f4271a3694d1414f0
George Bashi <jamil@georgebashi.com>
2011-09-24 19:25:29 +0100

    use sh magic to get dirname (thanks @mikepea!)

--- mghelper.sh
+++ mghelper.sh
@@ -3,14 +3,15 @@
 cwd=`pwd`
 parent_dir=$cwd
 
-while [ ! "/" = "$parent_dir" -a ! -r $parent_dir/.mg ]; do
-  parent_dir=`dirname $parent_dir`
+while [ ! -r $parent_dir/.mg ]; do
+  parent_dir=${parent_dir%/*}
+
+  if [ -z $parent_dir ]; then
+    echo " [mg] Can't find .mg dir!"
+    exit 1
+  fi
 done
 
-if [ "/" = $parent_dir ]; then
-  echo " [mg] Can't find .mg dir!"
-  exit 1
-fi
 
 did_run_helper=1
 . $parent_dir/.mg/mg/mg.sh
